c7b38594f0427883db87030412ffe1ea1e6933c8
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-11-16 15:39:36 -0500

    Added in Google API to lib folder.

---
+++ api/SnapDecision/Controllers/SearchController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parent5446
+ * Date: 11/16/13
+ * Time: 3:29 PM
+ */
+
+namespace SnapDecision\Controllers;
+
+/**
+ * Class SearchController
+ *
+ * @package SnapDecision\Controllers
+ */
+class SearchController
+{
+	/**
+	 * Fetch information from a URN
+	 *
+	 * @param array $params URL parameters
+	 * @param mixed $data Request data
+	 */
+	public function get( array $params, $data ) {
+
+	}
+} 
